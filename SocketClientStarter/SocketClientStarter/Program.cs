45a0959e3f128df70d792da4e3eb56d8c1c71790
kubaadamus <jakubadamus1991@gmail.com>
2018-09-17 21:43:31 +0200

    Klient i serwer łączą się. klient wysyla serwerowi byte arraya a ten mu ją odsyła

---
+++ SocketClientStarter/SocketClientStarter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClientStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = null;
+            IPAddress.TryParse("89.229.95.152", out ipaddr);
+            int intPort = 16010;
+            string inputString;
+
+            client.Connect(ipaddr, intPort);
+
+            while (true)
+            {
+                inputString = Console.ReadLine();
+
+                byte[] buffSend = Encoding.ASCII.GetBytes(inputString);
+
+                client.Send(buffSend);
+
+                byte[] buffReceived = new byte[128];
+                int numberOfReceivedBytes = client.Receive(buffReceived);
+
+                string receivedText = Encoding.ASCII.GetString(buffReceived, 0, numberOfReceivedBytes);
+
+                Console.WriteLine(receivedText);
+            }
+
+            if(client !=null)
+            {
+                if(client.Connected)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- SocketServerStarter/SocketServerStarter/Program.cs
+++ SocketServerStarter/SocketServerStarter/Program.cs
@@ -14,50 +14,32 @@ namespace SocketServerStarter
         static void Main(string[] args)
         {
             Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
             IPAddress ipaddr = IPAddress.Any;
-
             IPEndPoint ipep = new IPEndPoint(ipaddr, 16010);
-
             listenerSocket.Bind(ipep);
-
             listenerSocket.Listen(5);
-
-            Console.WriteLine("About to accept incoming connection");
-
-            Socket client = listenerSocket.Accept();
-
-            Console.WriteLine("Client connected. " + client.ToString() + " IP EndPoint: " + client.RemoteEndPoint.ToString());
-
+            int numberOfReceivedBytes = 0;
             byte[] buff = new byte[786432];
 
-            int numberOfReceivedBytes = 0;
+            Console.WriteLine("About to accept incoming connection");
+            Socket client = listenerSocket.Accept();
+            Console.WriteLine("Client connected. " + client.ToString() + " IP EndPoint: " + client.RemoteEndPoint.ToString());
 
-
-            while(true)
-            {
-                numberOfReceivedBytes = client.Receive(buff);
-
-                Console.WriteLine("number of received bytes" + numberOfReceivedBytes);
-
-                Console.WriteLine("Data sent by client is: " + buff);
-
-                string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
-
-                Console.WriteLine("Data sent by client: " + receivedText);
-
-                client.Send(buff);
-
-                if (receivedText == "quit")
+                while (true)
                 {
-                    break;
+                    numberOfReceivedBytes = client.Receive(buff); // tutaj czekaj na dane od klienta, ten element blokuje kod
+
+                    string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes); // zamien odebrane dane na string
+
+                    Console.WriteLine("Data sent by client: " + receivedText);
+
+                    client.Send(buff); //odeślij do klienta spowrotem
+
+                    Array.Clear(buff, 0, buff.Length);  // wyczyść buff i przygotuj na nowe dane
+                    numberOfReceivedBytes = 0;          //wyzeruj licznik odebranych bajtów
+
                 }
-
-                Array.Clear(buff, 0, buff.Length);
-                numberOfReceivedBytes = 0;
-
-
-            }
+           
             Console.ReadKey();
         }
     }
